c201541571cb521c1955547a8b38e9795cb959f4
diyar1 <diyarolmezz124@gmail.com>
2025-03-18 12:50:13 -0700

    Test case Uss_03 yazilmis fakat insan dogrulamasi nedeniyle test edilememistir.

---
+++ src/Uss_03_TC_0301/BaseDriver.java
@@ -0,0 +1,36 @@
+package Uss_03_TC_0301;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseDriver {
+
+    public static WebDriver driver;
+
+
+
+
+        static{
+
+        driver=new ChromeDriver();
+
+        driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // 20 sn mühlet: elementi bulma mühleti
+    }
+
+
+
+        public static void BekleKapat(){
+        MyFunc.bekle(1);
+        driver.quit();
+
+    }
+
+
+
+    }
+
+
